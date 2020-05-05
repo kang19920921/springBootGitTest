88ab8bca7686eb59846d8c3e214192ef2e3de3ad
Captkang <Captkang@192.168.2.1>
2020-05-05 15:21:00 +0800

    整合redis

--- springBootTestGit/src/main/java/cn/tedu/GitMain.java
+++ springBootTestGit/src/main/java/cn/tedu/GitMain.java
@@ -1,9 +1,11 @@
 package cn.tedu;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("cn.tedu.mapper")
 public class GitMain {
 	
 	public static void main(String[] args) {
