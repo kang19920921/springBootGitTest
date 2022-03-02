2d79cccc937cd81ad872de4415377558e4ae3cc2
kang19920921 <root132888>
2022-03-02 23:06:57 +0800

    增加AES加密工具类

--- springBootTestGit/src/main/java/cn/tedu/Utils/AESUtils.java
+++ springBootTestGit/src/main/java/cn/tedu/Utils/AESUtils.java
@@ -10,6 +10,7 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.transform.Source;
 import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
 @Slf4j
 @Getter
@@ -17,12 +18,11 @@ import java.nio.charset.StandardCharsets;
 public class AESUtils {
     private static final String KEY_ALGORITHM = "AES";
     /*** 默认的加密算法 */
-    private static final String DEFAULT_CIPHER_ALGORITHM = "AES/ECB/PKCS5Padding";
+    private static final String CIPHER_ALGORITHM = "AES/CBC/NOPadding";
     //向量
     private static final String IV_SEED = "1234567812345678";
 
     public static String encrypt(String str, String key) {
-
         try {
             if (null == str) {
                 log.info("AES加密出错:key为空null");
@@ -34,32 +34,32 @@ public class AESUtils {
                 return null;
             }
 
-            byte[]  raw = key.getBytes(StandardCharsets.UTF_8);
+            byte[] raw = key.getBytes(StandardCharsets.UTF_8);
             //创建获取秘钥
-            SecretKeySpec  secretKeySpec  =  new SecretKeySpec(raw,KEY_ALGORITHM);
+            SecretKeySpec secretKeySpec = new SecretKeySpec(raw, KEY_ALGORITHM);
             //创建密码器
-            Cipher  cipher  = Cipher.getInstance(DEFAULT_CIPHER_ALGORITHM);
-            IvParameterSpec  iv  = new IvParameterSpec(IV_SEED.getBytes(StandardCharsets.UTF_8));
+            Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+            IvParameterSpec iv = new IvParameterSpec(IV_SEED.getBytes(StandardCharsets.UTF_8));
             //密码器初始化
-            cipher.init(Cipher.ENCRYPT_MODE,secretKeySpec,iv);
-            byte[] srawt  = str.getBytes(StandardCharsets.UTF_8);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, iv);
+            byte[] srawt = str.getBytes(StandardCharsets.UTF_8);
             int len = srawt.length;
             //
             /** 计算补空格后的长度 **/
-            while (len  %  16 !=0){
+            while (len % 16 != 0) {
                 len++;
             }
-            byte [] sraw  =  new byte[len];
+            byte[] sraw = new byte[len];
             /** 在最后空格  **/
             for (int i = 0; i < len; ++i) {
-                if( i < srawt.length){
+                if (i < srawt.length) {
                     sraw[i] = srawt[i];
                 } else {
-                    sraw[i] = 32 ;
+                    sraw[i] = 32;
                 }
             }
             byte[] encypted = cipher.doFinal(sraw);
-           return  formatString(new String(Base64.encodeBase64(encypted),StandardCharsets.UTF_8));
+            return formatString(new String(Base64.encodeBase64(encypted), StandardCharsets.UTF_8));
 
         } catch (Exception e) {
             log.error("AES加密出错： " + e.toString());
@@ -68,12 +68,62 @@ public class AESUtils {
         }
     }
 
+
+    /**
+     * AES解密算法
+     *
+     * @param str
+     * @param key
+     * @return
+     */
+    public static String decrypt(String str, String key) {
+        try {
+            if (null == str) {
+                log.info("AES解密出错:key为空null");
+                return null;
+            }
+            //判断key是否16位
+            if (key.length() != 16) {
+                log.info("AES解密出错:key不是16位");
+                return null;
+            }
+            //创建秘钥
+            byte[] raw = key.getBytes(StandardCharsets.UTF_8);
+            SecretKeySpec secretKeySpec = new SecretKeySpec(raw, KEY_ALGORITHM);
+            //创建密码器
+            Cipher cipher = Cipher.getInstance(CIPHER_ALGORITHM);
+            IvParameterSpec iv = new IvParameterSpec(IV_SEED.getBytes(StandardCharsets.UTF_8));
+            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, iv);
+            byte[] bytes = Base64.decodeBase64(str.getBytes(StandardCharsets.UTF_8));
+            bytes = cipher.doFinal(bytes);
+            return new String(bytes, StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            log.error("AES解密出错： " + e.toString());
+            return null;
+        }
+
+    }
+
     private static String formatString(String source) {
         if (null == source) {
             return null;
         }
 
         return source.replaceAll("\\r", "").replaceAll("\\n", "");
+    }
+
+
+    public static void main(String[] args) {
+        String aeskey = UUIDUtils.getUUID16();
+        String source = "第一次AES加解密------=======------";
+        //加密
+        String encrypt_str = encrypt(source, aeskey);
+        log.info("encrypt_str={}", encrypt_str);
+
+        //解密
+        String dencrypt_str = decrypt(encrypt_str, aeskey);
+        log.info("dencrypt_str={}", dencrypt_str);
+
     }
 
 
